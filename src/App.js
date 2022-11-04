1664f9fc97f9cb93bc7c2844d012b81f05b87ba0
Deacon-debug <>
2022-11-04 12:55:32 +0200

    displayed componets

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
+import Feed from './componets/Feed';
+import Post from './componets/Post'
 function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">
       Hello world!
+      
     </h1>
+      <Feed />
     </div>
   );
 }
