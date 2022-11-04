850c0e98daec950abff68845990813d93f3aac31
Deacon-debug <>
2022-11-04 12:55:06 +0200

    created components

---
+++ src/componets/Feed.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function feed() {
+  return (
+    <div>
+        feed
+        hwuwr
+    </div>
+  )
+}
+
+export default feed
---
+++ src/componets/Post.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function post() {
+  return (
+    <div>
+        {DB}
+    </div>
+  )
+}
+
+export default post
