2c1e13e51ecc0c4fd5eac51d3a6782736b30bf54
Deacon-debug <>
2022-11-05 15:55:21 +0200

    customized font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  darkMode:'class',
   theme: {
-    extend: {},
+    
+    extend: {
+      fontSize: {
+      sm: '0.78rem',
+      base: '1rem',
+      xl: '1.25rem',
+    }
+    },
   },
   plugins: [],
 }
