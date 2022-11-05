67c3870f27284c8c63794b33a5c2b56cc10ecfa6
Deacon-debug <>
2022-11-05 15:53:08 +0200

    added posts in feed

--- src/componets/Feed.js
+++ src/componets/Feed.js
@@ -1,10 +1,18 @@
 import React from 'react'
-
+import Post from './Post'
+import data from '../DB.json'
 function feed() {
   return (
     <div>
-        feed
-        hwuwr
+        {data.map((item,index)=>{
+            return(
+                <div key={index} className="pb-10">
+                 <Post user={item.name} content={item.content} />
+                </div>
+            )
+       
+        })}
+      
     </div>
   )
 }
